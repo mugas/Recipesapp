8ac73bc65eee9074754ddfd154cc511a36a94216
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-05-05 05:59:24 +0300

    Adding state

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 import Search from "./models/Search";
 
+/* Global state of the app
+ * -Search Object
+ * -Current recipe object
+ * -Shopping list object
+ *- Liked recipe
+ */
+const state = {};
+
 const search = new Search("pizza");
 console.log(search);
 search.getResults();

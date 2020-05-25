869c6ed9c01b2b48e61f9ee4d3c609e817e19dae
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-05-25 09:55:12 +0300

    add recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -65,6 +65,10 @@ const controlRecipe = async () => {
     recipeView.clearRecipe();
     renderLoader(elements.recipe);
 
+    // Highlight selected search item
+
+    if (state.search) searchView.highlightSelected(id);
+
     // Create new recipe object
     state.recipe = new Recipe(id);
 
@@ -94,3 +98,20 @@ const controlRecipe = async () => {
 ["haschange", "click"].forEach((event) =>
   window.addEventListener(event, controlRecipe)
 );
+
+// Handling recipe button clicks
+elements.recipe.addEventListener("click", (e) => {
+  if (e.target.matches(".btn-decrease", "btn-decrease *")) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings("dec");
+      recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches(".btn-increase, .btn-increase*")) {
+      // Increase button is clicked
+      state.recipe.updateServings("inc");
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  }
+
+  console.log(state.recipe);
+});
